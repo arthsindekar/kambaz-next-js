b8dcb80b7088bd478e0e20155f72a19198777397
arthsindekar <sindekar.a@northeastern.edu>
2025-09-20 22:24:09 -0400

    added name and links

--- src/app/Labs/page.tsx
+++ src/app/Labs/page.tsx
@@ -2,6 +2,18 @@ import Link from "next/link";
 export default function Labs() {
   return (
     <div id="wd-labs">
+      <h1>Arth Sindekar</h1>
+      <h3>Section:04</h3>
+      <h3>CRN: 18616 </h3>
+      <h3>Links:</h3>
+      <ul>
+        <li>
+          <Link href="https://github.com/arthsindekar/kambaz-next-js">GitHub Source Code Link</Link>
+        </li>
+        <li>
+          <Link href="https://kambaz-next-js-o1g1.vercel.app/">Click Here for Kambaz App</Link>
+        </li>
+      </ul>
       <h1>Labs</h1>
       <ul>
         <li>
