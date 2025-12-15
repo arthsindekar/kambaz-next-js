e4c8884bb7f7929be21d03aa4239f2ca92c8c6c2
arthsindekar <sindekar.a@northeastern.edu>
2025-12-15 13:54:38 -0500

    eslint turned off

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,20 +6,26 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+    baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
-    ],
-  },
+    ...compat.extends("next/core-web-vitals", "next/typescript"),
+    {
+        rules: {
+            "react/no-unescaped-entities": "off",
+            "@typescript-eslint/no-explicit-any": "off",
+        },
+    },
+    {
+        ignores: [
+            "node_modules/**",
+            ".next/**",
+            "out/**",
+            "build/**",
+            "next-env.d.ts",
+        ],
+    },
 ];
 
 export default eslintConfig;
